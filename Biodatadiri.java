2ad23f055b7efccbaed5b49dad5eedaaef7e79d3
Mohammad Alka. R <99939707+alfkarmdhn@users.noreply.github.com>
2022-04-04 21:34:30 +0700

    Create Biodatadiri.java

---
+++ Biodatadiri.java
@@ -0,0 +1,21 @@
+
+package mohammad.alka;
+
+public class BiodataDiri {
+    public static void main(String[]args){
+        String nama= "Mohammad Alka Ramadhan";
+        String nis="542211169";
+        String tempat_lahir="Kediri";
+        String tanggal_lahir="29 oktober 2005";
+        String jenis_kelamin="laki laki";
+        String alamat_mlg="Jl. Danau Ranau X Blok G7 D RT 02 RW 03 Sawojajar Malang 65139";
+        String motto_hidup="God - Courage - Success";
+        System.out.println("nama             : " +nama);
+        System.out.println("NIS              : " +nis);
+        System.out.println("tempat lahir     : " +tempat_lahir);
+        System.out.println("tanggal lahir    : " +tanggal_lahir);
+        System.out.println("jenis kelamin    : " +jenis_kelamin);
+        System.out.println("alamat di malang : " +alamat_mlg);
+        System.out.println("motto hidup      : " +motto_hidup);
+    }
+}
